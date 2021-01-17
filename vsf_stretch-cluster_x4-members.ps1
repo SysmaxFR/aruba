4da1d446ddd91e75be32e11f152464996f11e988
SysmaxFR <68464968+SysmaxFR@users.noreply.github.com>
2021-01-17 18:17:24 +0100

    Update and rename test_pwsh7.ps1 to vsf_stretch-cluster_x4-members.ps1

--- vsf_stretch-cluster_x4-members.ps1
+++ vsf_stretch-cluster_x4-members.ps1
@@ -1,7 +1,8 @@
-﻿# pwsh v7
+# Fonctionne seulement avec powershell v7+.
 if(!($PSVersionTable.PSVersion.Major -eq "7")){ Write-Host "Script use only PwSH v7"; exit 0 }
 
-
+# Definition des variables.
+# $AutoRemediation=$true permet de re-equilibrage du Commander/Standby en fonction du Stretch-Cluster.
 $IP = "X.X.X.X"
 $Rest = "rest/v7"
 $AutoRemediation = $false
@@ -10,34 +11,36 @@ $Login = @{
     "password" = "monsupermdp"
 }
 
-
+# Definition des commandes AnyCLI.
 $VsfMember1 = @{ "cmd" = "show vsf member 1" }
 $VsfMember2 = @{ "cmd" = "show vsf member 2" }
 $VsfMember3 = @{ "cmd" = "show vsf member 3" }
 $VsfMember4 = @{ "cmd" = "show vsf member 4" }
 $VsfRedundancy = @{ "cmd" = "redundancy switchover" }
 
-
+# Ouverture de la session RestAPI, recuperation d'un cookie d'authentification.
 $Session = Invoke-RestMethod -Uri "http://$IP/$Rest/login-sessions" -Method 'POST' -ContentType 'application/json' -SessionVariable 'Cookie' -Body ($Login|ConvertTo-Json)
 
-#$Vlans = Invoke-RestMethod -Uri "http://$IP/$Rest/vlans" -Method GET -ContentType 'application/json' -WebSession $Cookie
+# Recuperation des resultats des commandes effectues sur le stack.
 $VsfMember1 = Invoke-RestMethod -Uri "http://$IP/$Rest/cli" -Method POST -ContentType 'application/json' -WebSession $Cookie -Body ($VsfMember1|ConvertTo-Json)
 $VsfMember2 = Invoke-RestMethod -Uri "http://$IP/$Rest/cli" -Method POST -ContentType 'application/json' -WebSession $Cookie -Body ($VsfMember2|ConvertTo-Json)
 $VsfMember3 = Invoke-RestMethod -Uri "http://$IP/$Rest/cli" -Method POST -ContentType 'application/json' -WebSession $Cookie -Body ($VsfMember3|ConvertTo-Json)
 $VsfMember4 = Invoke-RestMethod -Uri "http://$IP/$Rest/cli" -Method POST -ContentType 'application/json' -WebSession $Cookie -Body ($VsfMember4|ConvertTo-Json)
 
-
+# Resultat recu en Base64, decodage.
 $VsfMember1 = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($VsfMember1.result_base64_encoded))
 $VsfMember2 = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($VsfMember2.result_base64_encoded))
 $VsfMember3 = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($VsfMember3.result_base64_encoded))
 $VsfMember4 = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($VsfMember4.result_base64_encoded))
 
-
-$StatusVsfMember1 = (($VsfMember1 -split "`n") | ? {$_ -match "status"}) | ConvertFrom-StringData -Delimiter ":"  #Convert the String to an array of string, then filter
+# Recuperation des status "Commander", "Standby", "Member" par membres du stack. Conversion pour faciliter l'usage avec powershell
+# Convert to String to Array of String, then filter.
+$StatusVsfMember1 = (($VsfMember1 -split "`n") | ? {$_ -match "status"}) | ConvertFrom-StringData -Delimiter ":" 
 $StatusVsfMember2 = (($VsfMember2 -split "`n") | ? {$_ -match "status"}) | ConvertFrom-StringData -Delimiter ":"
 $StatusVsfMember3 = (($VsfMember3 -split "`n") | ? {$_ -match "status"}) | ConvertFrom-StringData -Delimiter ":"
 $StatusVsfMember4 = (($VsfMember4 -split "`n") | ? {$_ -match "status"}) | ConvertFrom-StringData -Delimiter ":"
 
+# Recuperation des links actifs par membres du stack. Par defaut 2 actifs.
 $LinksVsfMember1 = (($VsfMember1 -split "`n") | ? {$_ -match "Active, Peer member"}) | Measure-Object -Line
 $LinksVsfMember2 = (($VsfMember2 -split "`n") | ? {$_ -match "Active, Peer member"}) | Measure-Object -Line
 $LinksVsfMember3 = (($VsfMember3 -split "`n") | ? {$_ -match "Active, Peer member"}) | Measure-Object -Line
